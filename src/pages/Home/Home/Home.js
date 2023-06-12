7b7cf12337e8dfb7d916e320616385414b68edfb
Atiqur-Rahman <rahmanatique88@gmail.com>
2023-06-12 22:02:56 +0600

    search button is done

--- src/pages/Home/Home/Home.js
+++ src/pages/Home/Home/Home.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import './Home.css';
 
 const Home = () => {
     return (
         <div>
-            <h2>This is home</h2>
+            <h3 className="text-center fw-bolder mt-5">I GROW BY HELPING PEOPLE IN NEED.</h3>
+
+            <div className="d-flex justify-content-center">
+                <form className="d-flex justify-content-center search-container" role="search">
+                    <input className="form-control search-field" type="search" placeholder="Search" aria-label="Search" />
+                    <button className="search-btn bg-primary text-white border border-0 rounded-end-3" type="submit">
+                        <span className="">Search</span>
+                    </button>
+                </form>
+            </div>
         </div>
     );
 };
--- src/pages/shared/Header/Header.js
+++ src/pages/shared/Header/Header.js
@@ -5,38 +5,36 @@ import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
-        <div>
-            <Navbar collapseOnSelect expand="lg" bg="light" variant="light">
-                <Container>
-                    <Navbar.Brand as={Link} to="/" className="lh-1 logo">
-                        <span className="fs-3 fw-semibold text-success m-0">Green</span> <br /> <span className="fs-4 m-0 pt-0">Organization</span>
-                    </Navbar.Brand>
-                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-                    <Navbar.Collapse id="responsive-navbar-nav">
-                        <Nav className="ms-auto d-flex align-items-center">
-                            <Nav.Link as={Link} to="/">
-                                Home
-                            </Nav.Link>
-                            <Nav.Link as={Link} to="/donation">
-                                Donation
-                            </Nav.Link>
-                            <Nav.Link as={Link} to="/events">
-                                Events
-                            </Nav.Link>
-                            <Nav.Link as={Link} to="/blog">
-                                Blog
-                            </Nav.Link>
-                            <Nav.Link as={Link} to="/register">
-                                <button className="bg-primary text-white border rounded-1 px-4 py-1 ">Register</button>
-                            </Nav.Link>
-                            <Nav.Link as={Link} to="/login">
-                                <button className=" text-primary fw-bold border border-2 border-primary rounded-1 px-4 py-1 ">Login</button>
-                            </Nav.Link>
-                        </Nav>
-                    </Navbar.Collapse>
-                </Container>
-            </Navbar>
-        </div>
+        <Navbar collapseOnSelect expand="md" bg="light" variant="light" className="p-0">
+            <Container className="p-0">
+                <Navbar.Brand as={Link} to="/" className="lh-1 logo">
+                    <span className="fs-3 fw-semibold text-success m-0">Green</span> <br /> <span className="fs-4 m-0 pt-0">Organization</span>
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav className="ms-auto d-flex align-items-center">
+                        <Nav.Link as={Link} to="/">
+                            Home
+                        </Nav.Link>
+                        <Nav.Link as={Link} to="/donation">
+                            Donation
+                        </Nav.Link>
+                        <Nav.Link as={Link} to="/events">
+                            Events
+                        </Nav.Link>
+                        <Nav.Link as={Link} to="/blog">
+                            Blog
+                        </Nav.Link>
+                        <Nav.Link as={Link} to="/register">
+                            <button className="bg-primary text-white border rounded-1 px-4 py-1 ">Register</button>
+                        </Nav.Link>
+                        <Nav.Link as={Link} to="/login">
+                            <button className=" text-primary fw-bold border border-2 border-primary rounded-1 px-4 py-1 ">Login</button>
+                        </Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
     );
 };
 
